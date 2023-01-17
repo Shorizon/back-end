3b3b2e9e061bcb5057934238d6c0975250ff91a8
shojib deb <shodeb123@gmail.com>
2023-01-17 11:22:17 +0000

    add goat filter

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 // Describes the server/API
 
+const e = require("express");
 const express = require("express"); // Access to the Express library
 
-const {goats,nextId} = require("./goats");
+const { goats, nextId } = require("./goats");
 
 const app = express(); // Make a very basic server using Express
 
@@ -18,4 +19,14 @@ app.get("/goats", (req, res) => {
     res.json(goats)
 })
 
+app.get("/goats/:id", (req, res) => {
+    const id = req.params["id"];
+    const goat = goats.filter(g => g["id"] == id)[0];
+
+    if (goat) {
+        res.json(goat)
+    } else {
+        res.status(404).json("no such goat!")
+    }
+})
 module.exports = app; // Make the server available to other files
