7da620c112fd0715a78d28a164cbbb413642b824
shojib deb <shodeb123@gmail.com>
2023-01-17 10:54:21 +0000

    setup goats

---
+++ server/goats.js
@@ -0,0 +1,28 @@
+const goats = [
+    {
+        "id":1,
+        "name": "Billy",
+        "age": 6,
+        "sex": "M",
+        "favouriteColour": "magenta"
+    },
+    {
+        
+        "id":2,
+        "name": "Beehronica",
+        "age": 23,
+        "sex": "F",
+        "favouriteColour": "cerulean"
+    },
+    {
+        
+        "id":3,
+        "name": "Beehmoth",
+        "age": 45,
+        "sex": "M",
+        "favouriteColour": "white"
+    }
+]
+
+let nextId = 4;
+module.exports = {goats,nextId};
